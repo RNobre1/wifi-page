fef059141a2c51b7b241608b4c4075c09e05d3a2
Rafael Nobre <scorpionbr30@gmail.com>
2025-09-25 17:22:35 -0300

    primeiro commit

---
+++ App.tsx
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import WifiCard from './components/WifiCard';
+
+const App: React.FC = () => {
+  return (
+    <main className="bg-gray-900 min-h-screen w-full flex items-center justify-center p-4 font-sans">
+      <WifiCard />
+    </main>
+  );
+};
+
+export default App;
---
+++ constants.ts
@@ -0,0 +1,5 @@
+
+export const SPONSOR_NAME = 'Barbearia do João';
+export const SPONSOR_LOGO_URL = 'https://picsum.photos/seed/barbershop/200';
+export const WIFI_SSID = 'BarbeariaJoao_WiFi_Gratis';
+export const WIFI_PASSWORD = 'FiqueEstilosoComNossoWiFi!';
