0b182cef1fe691de68aed690a6671dc13a8f3b32
Rafael Nobre <scorpionbr30@gmail.com>
2025-09-25 17:52:47 -0300

    add: rede da Emater

--- constants.ts
+++ constants.ts
@@ -1,5 +1,5 @@
 
-export const SPONSOR_NAME = 'Barbearia do João';
+export const SPONSOR_NAME = 'Valmar Kaba';
 export const SPONSOR_LOGO_URL = 'https://picsum.photos/seed/barbershop/200';
-export const WIFI_SSID = 'BarbeariaJoao_WiFi_Gratis';
-export const WIFI_PASSWORD = 'FiqueEstilosoComNossoWiFi!';
+export const WIFI_SSID = 'EMATER-Movél';
+export const WIFI_PASSWORD = 'Emater2025@';
